f86a8e13433e67158ef23d85672ddef80417268c
Jangwon <wkddnjset@gmail.com>
2020-08-12 21:35:48 +0900

    Fix Mobx

--- pages/count.tsx
+++ pages/count.tsx
@@ -1,30 +1,32 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useContext } from "react";
 import Link from "next/link";
-import { useObserver } from "mobx-react";
 
 import Layout from "@components/Layout";
-import { useStores } from "@utils/Mobx";
+
+import { observer } from "mobx-react-lite";
+import CountStore from "@stores/Count";
 
 type Props = {
   children?: ReactNode;
 };
 
-const AboutPage: React.FC<Props> = () => {
-  const { Count } = useStores();
-  return useObserver(() => (
+const AboutPage: React.FC<Props> = observer(() => {
+  const countStore = useContext(CountStore);
+
+  return (
     <Layout title="About | Next.js + TypeScript Example">
       <h1>Count</h1>
       <p>This is the count page</p>
-      {Count.number}
-      <button onClick={Count.increase}>+</button>
-      <button onClick={Count.decrease}>-</button>
+      {countStore.number}
+      <button onClick={countStore.increase}>+</button>
+      <button onClick={countStore.decrease}>-</button>
       <p>
         <Link href="/">
           <a>Go home</a>
         </Link>
       </p>
     </Layout>
-  ));
-};
+  );
+});
 
 export default AboutPage;
--- stores/Count.tsx
+++ stores/Count.tsx
@@ -1,3 +1,4 @@
+import { createContext } from "react";
 import { observable, action } from "mobx";
 
 class Count {
@@ -11,4 +12,5 @@ class Count {
   };
 }
 
-export default new Count();
+const countStore = new Count();
+export default createContext(countStore);
