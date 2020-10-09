62d8e0669fa79c27e72cb27ad731b9112b1c2a56
Jangwon <wkddnjset@naver.com>
2020-10-10 01:26:19 +0900

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -43,10 +43,12 @@ export default class MyDocument extends Document<Props> {
             rel="stylesheet"
             type="text/css"
           />
+          <link rel="canonical" href="https://rocketmakers.io/" />
           <meta
             name="viewport"
             content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"
           />
+          <meta name="description" content={SITE_DESCRIPTION} />
           <meta property="og:type" content="website" />
           <meta property="og:site_name" content={SITE_NAME} />
           <meta property="og:title" content={SITE_TITLE} />
