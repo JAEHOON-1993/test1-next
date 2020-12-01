f9bed0e25a945605130d304467536301555a2fd1
Jangwon <wkddnjset@gmail.com>
2020-12-01 17:03:45 +0900

    [Fix] Nav Component in/Visible Icons

--- components/Nav/nav_mo.tsx
+++ components/Nav/nav_mo.tsx
@@ -23,8 +23,8 @@ type Props = {
   style?: any;
 };
 
-const NoBackPath = ["/chat"];
-const NoSearchPath = ["/", "/indoor", "/outdoor"];
+const NO_BACK_PATH = ["/", "/chat"];
+const SEARCH_PATH = ["/", "/indoor", "/outdoor"];
 
 const Nav: React.FC<Props> = ({ pageName, transparent }) => {
   const router = useRouter();
@@ -32,6 +32,9 @@ const Nav: React.FC<Props> = ({ pageName, transparent }) => {
   const [fixed, setFixed] = useState<boolean>(false);
   useEffect(() => {
     setPath(router.pathname);
+    console.log("router.pathname : ", router.pathname);
+    console.log(NO_BACK_PATH.indexOf(path));
+    console.log(SEARCH_PATH.indexOf(path));
   }, [router]);
   const listener = () => {
     if (document.body.getBoundingClientRect().top === 0) {
@@ -45,24 +48,21 @@ const Nav: React.FC<Props> = ({ pageName, transparent }) => {
     return () => {
       window.removeEventListener("scroll", listener);
     };
-  });
+  }, []);
   return (
     <>
       <NavWrap transparent={transparent} fixed={fixed}>
         <CustomContainer>
-          {path != "/" ||
-            (NoBackPath.indexOf(path) > 0 && (
-              <MobileRightBox onClick={router.back}>
-                <Icon
-                  name="arrow-left"
-                  color={
-                    transparent && !fixed
-                      ? theme.color.WHITE
-                      : theme.color.GRAY5
-                  }
-                />
-              </MobileRightBox>
-            ))}
+          {NO_BACK_PATH.indexOf(path) < 0 && (
+            <MobileRightBox onClick={router.back}>
+              <Icon
+                name="arrow-left"
+                color={
+                  transparent && !fixed ? theme.color.WHITE : theme.color.GRAY5
+                }
+              />
+            </MobileRightBox>
+          )}
           {pageName ? (
             <PageName lg>{pageName}</PageName>
           ) : (
@@ -72,7 +72,7 @@ const Nav: React.FC<Props> = ({ pageName, transparent }) => {
             />
           )}
           <MobileLeftBox>
-            {NoSearchPath.indexOf(path) > 0 && (
+            {SEARCH_PATH.indexOf(path) > -1 && (
               <Icon
                 style={{ margin: 0 }}
                 name="search"
