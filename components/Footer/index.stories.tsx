8bbfe9f7f11f3436f13fb6f2833b871896d2775b
hokyunrhee <hokyun.rhee@gmail.com>
2021-03-23 10:29:01 +0900

    [Improve] Footer 컴포넌트 스토리북 Docs 보완

--- components/Footer/index.stories.tsx
+++ components/Footer/index.stories.tsx
@@ -4,16 +4,56 @@ import { Story, Meta } from '@storybook/react';
 
 import Footer, { Props } from './index';
 
+import styled from 'styled-components';
+
 export default {
   title: 'Example/components/Footer',
   component: Footer,
-  parameters: {
-    componentSubtitle:
-      'Footer 컴포넌트는 웹 사이트의 부가적인 정보들을 표시하기 위해 사용됩니다.',
-  },
 } as Meta;
 
 const Template: Story<Props> = (args) => <Footer {...args} />;
 
 export const Deafult = Template.bind({});
 Deafult.args = {};
+
+const Wrapper = styled.div`
+  .description {
+    margin-bottom: 12px;
+  }
+  & > div + div {
+    margin-top: 32px;
+  }
+`;
+
+export const IconFooter = () => (
+  <Wrapper>
+    <div className="description">Icon Footer</div>
+    <Footer />
+  </Wrapper>
+);
+
+export const TextFooter = () => (
+  <Wrapper>
+    <div className="description">Text Footer</div>
+    <Footer iconMode={false} />
+  </Wrapper>
+);
+
+export const Colors = () => {
+  return (
+    <Wrapper>
+      <div>
+        <div className="description">Dark</div>
+        <Footer />
+      </div>
+      <div>
+        <div className="description">Light</div>
+        <Footer colorMode="light" />
+      </div>
+      <div>
+        <div className="description">Primary</div>
+        <Footer colorMode="primary" />
+      </div>
+    </Wrapper>
+  );
+};
--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -8,21 +8,21 @@ import TextFooter from './TextFooter';
 
 export interface Props {
   /**
-   * Footer는 2가지 타입이 제공됩니다. 기본은 'icon footer'입니다.
-   *
-   * `icon footer`는 SNS, 홈페이지 링크 연결 등의 아이콘을 제공합니다.
-   *
-   * `text footer`는 텍스트 정보로만 구성되어 있습니다.
+   * Footer는 2가지 타입이 제공됩니다. 기본은 icon footer입니다.
    */
   iconMode?: boolean;
   /**
-   * Footer는 3가지 색상 모드가 제공됩니다. 기본은 dark'입니다.
+   * Footer는 3가지 색상 모드가 제공됩니다. 기본은 dark입니다.
    */
   colorMode?: 'dark' | 'light' | 'primary';
 }
 
 /**
- * Footer의 텍스트, 아이콘 등의 수정은 `IconFooter`와 `TextFooter`에서 할 수 있습니다.
+ * Footer 컴포넌트는 웹 사이트의 부가적인 정보들을 표시하기 위해 사용됩니다. 텍스트, 아이콘 등의 수정은 `IconFooter`와 `TextFooter`에서 할 수 있습니다.
+ *
+ * `Icon footer`는 SNS, 홈페이지 링크 연결 등의 아이콘을 제공합니다.
+ *
+ * `Text footer`는 텍스트 정보로만 구성되어 있습니다.
  */
 const Footer: React.FC<Props> = ({ iconMode = true, colorMode = 'dark' }) => {
   return (
