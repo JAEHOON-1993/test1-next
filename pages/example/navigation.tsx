9a068a5dec82b6b17a7812e3df1983d3ab7a3db2
Jangwon <wkddnjset@gmail.com>
2021-03-25 21:51:20 +0900

    [Fix] fixed to build

--- pages/example/navigation.tsx
+++ pages/example/navigation.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import NavType1Container from 'components/Header/example/TextHeader_1';
+// import NavType1Container from 'components/Header/example/TextHeader_1';
 import NavType2Container from 'components/Header/example/TextHeader_2';
 import NavType3Container from 'components/Header/example/TextHeader_3';
 import NavType4Container from 'components/Header/example/TextHeader_4';
@@ -9,7 +9,7 @@ import NavType5Container from 'components/Header/example/TextHeader_5';
 const NavigationPage = () => {
   return (
     <div style={{ width: '100%', height: '100vh', backgroundColor: '#ddd' }}>
-     {/* <NavType1Container /> */}
+      {/* <NavType1Container /> */}
       <div style={{ height: 10 }} />
       <NavType2Container />
       <div style={{ height: 10 }} />
