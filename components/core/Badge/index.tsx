941c9ddd977dc76bdca43a5e901e70405e299921
Young Joo Kim <feel2174@gmail.com>
2021-03-29 12:18:37 +0900

    [Fix]fixed small size width

--- components/core/Badge/index.tsx
+++ components/core/Badge/index.tsx
@@ -10,21 +10,18 @@ interface Props {
 const sizes = {
   default: {
     pc: css`
-      //width: 53px;
       height: 30px;
       font-size: 12px;
       line-height: 18px;
       padding: 0px 15px;
     `,
     tab: css`
-      //width: 49px;
       height: 30px;
       font-size: 10px;
       line-height: 16px;
       padding: 0px 15px;
     `,
     mobile: css`
-      //width: 53px;
       height: 30px;
       font-size: 12px;
       line-height: 18px;
@@ -33,21 +30,18 @@ const sizes = {
   },
   small: {
     pc: css`
-      width: 39px;
       height: 26px;
       font-size: 12px;
       line-height: 18px;
       padding: 0px 8px;
     `,
     tab: css`
-      width: 35px;
       height: 26px;
       font-size: 10px;
       line-height: 16px;
       padding: 0px 8px;
     `,
     mobile: css`
-      width: 39px;
       height: 26px;
       font-size: 12px;
       line-height: 18px;
