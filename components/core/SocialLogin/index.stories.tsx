cfceb9a0e39987d4f8160a20d0d73c0a57d20bbf
Young Joo Kim <feel2174@gmail.com>
2021-03-29 17:03:47 +0900

    [Add]added Apple Icon in Icon Component

--- components/core/SocialLogin/index.stories.tsx
+++ components/core/SocialLogin/index.stories.tsx
@@ -3,46 +3,39 @@ import styled from 'styled-components';
 
 import { Meta } from '@storybook/react/types-6-0';
 
-import KakaoLogin from './KakaoLogin'
-import NaverLogin from './NaverLogin'
-import FacebookLogin from './FacebookLogin'
-import GoogleLogin from './GoogleLogin'
-import AppleLogin from './AppleLogin'
+import KakaoLogin from './KakaoLogin';
+import NaverLogin from './NaverLogin';
+import FacebookLogin from './FacebookLogin';
+import GoogleLogin from './GoogleLogin';
+import AppleLogin from './AppleLogin';
 
 export default {
-    title: 'Components/Core/SocialLogin',
-
-  } as Meta;
+  title: 'Components/Core/SocialLogin',
+} as Meta;
 
 export const socialButton = () => {
-    return (
-      <>
-        <SocialBox>
-          <KakaoLogin />
-          <NaverLogin />
-          <FacebookLogin />
-          <GoogleLogin />
-          <AppleLogin />
-        </SocialBox>
-      </>
-    );
-  };
-  
-  const SocialBox = styled.div`
-    > button {
-      margin-bottom: 8px;
-    }
-  `;
-  
+  return (
+    <>
+      <SocialBox>
+        <KakaoLogin />
+        <NaverLogin />
+        <FacebookLogin />
+        <GoogleLogin />
+        <AppleLogin />
+      </SocialBox>
+    </>
+  );
+};
 
-
-
+const SocialBox = styled.div`
+  > button {
+    margin-bottom: 8px;
+  }
+`;
 socialButton.parameters = {
-    docs: {
-      description: {
-        story:
-          '소셜 로그인 기능을 제공합니다.',
-      },
+  docs: {
+    description: {
+      story: '소셜 로그인 기능을 제공합니다.',
     },
-  };
-  
+  },
+};
